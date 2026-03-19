41c5296a80a7ac52be33348c6e36fad3d655ba38
vaibhavrank <vaibhavrank20@gmail.com>
2026-03-20 00:49:41 +0530

    Database enabled

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config.settings import settings
 
-engine = create_engine(settings.database_url)
+engine = create_engine(settings.database_url, connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(
     autocommit=False,
